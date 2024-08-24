b317127f416c9ba8db4489c66cd55a03b780f484
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2024-08-24 23:12:24 +0900

    [modify] Update Invoke AI files

--- start.sh
+++ start.sh
@@ -10,29 +10,25 @@ podman build -t container-download \
 # automatic1111
 podman build -t container-automatic1111 \
   --force-rm \
-  --build-arg CLI_ARGS="--allow-code --medvram --xformers --enable-insecure-extension-access --api" \
   --volume "/opt/stable-diffusion-webuis-podman/data:/data" \
   --volume "/opt/stable-diffusion-webuis-podman/output:/output" \
   --device "nvidia.com/gpu=all" \
   ./services/AUTOMATIC1111
 
-# invoke
-podman build -t container-invoke \
+# invokeai
+podman build -t container-invokeai \
   --force-rm \
-  --build-arg PRELOAD=true \
-  --build-arg CLI_ARGS="--xformers" \
   --volume "/opt/stable-diffusion-webuis-podman/data:/data" \
   --volume "/opt/stable-diffusion-webuis-podman/output:/output" \
   --device "nvidia.com/gpu=all" \
-  ./services/invoke/
+  ./services/invokeai/
 
 # comfy
 podman build -t container-comfy \
   --force-rm \
-  --build-arg CLI_ARGS="" \
   --volume "/opt/stable-diffusion-webuis-podman/data:/data" \
   --volume "/opt/stable-diffusion-webuis-podman/output:/output" \
   --device "nvidia.com/gpu=all" \
   ./services/comfy/
 
-podman play kube --replace k8s.yaml
+podman play kube --replace webuis.yaml
